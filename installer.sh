3c9a99599c05ba5de724f5fdf50329f08f2c6c20
Linkfy <linkfy@linkfys-mbp-3.lan>
2022-06-25 21:32:59 +0200

    Mod oh-my-zshOC

--- installer.sh
+++ installer.sh
@@ -60,7 +60,7 @@ if [ -d ~/.oh-my-zsh ]; then
 	echo -e "\n[-] oh-my-zsh Already Installed"
  else
  	echo -e "\n[+] Installing oh-my-zsh"
- 	sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+ 	newtabi 'sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"'
 fi
 
 
